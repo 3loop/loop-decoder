293c282587e28a638de53b5fcd34473c727dbcc5
Anastasia Rodionova <argali96@gmail.com>
2025-01-09 21:18:48 +0800

    Test updates

--- packages/transaction-decoder/test/mocks/create-mock.js
+++ packages/transaction-decoder/test/mocks/create-mock.js
@@ -1,60 +1,59 @@
 import fs from 'node:fs'
 import { createPublicClient, http } from 'viem'
-
-const RPC = 'https://rpc.ankr.com/eth'
+import { RPC } from '../constants.js'
 
 BigInt.prototype.toJSON = function () {
-    return this.toString()
+  return this.toString()
 }
 
 async function createMock(hash, rpcUrl) {
-    const publicClient = createPublicClient({
-        transport: http(rpcUrl),
-    })
+  const publicClient = createPublicClient({
+    transport: http(rpcUrl),
+  })
 
-    const [receipt, transaction, trace] = await Promise.all([
-        publicClient.request({ method: 'eth_getTransactionReceipt', params: [hash] }),
-        publicClient.request({ method: 'eth_getTransactionByHash', params: [hash] }),
-        publicClient.request({ method: 'trace_transaction', params: [hash] }),
-    ])
+  const [receipt, transaction, trace] = await Promise.all([
+    publicClient.request({ method: 'eth_getTransactionReceipt', params: [hash] }),
+    publicClient.request({ method: 'eth_getTransactionByHash', params: [hash] }),
+    publicClient.request({ method: 'trace_transaction', params: [hash] }),
+  ])
 
-    if (transaction == null) return
+  if (transaction == null) return
 
-    const block = transaction.blockNumber
-        ? await publicClient.request({ method: 'eth_getBlockByNumber', params: [transaction.blockNumber, true] })
-        : undefined
+  const block = transaction.blockNumber
+    ? await publicClient.request({ method: 'eth_getBlockByNumber', params: [transaction.blockNumber, true] })
+    : undefined
 
-    return {
-        receipt,
-        transaction,
-        trace,
-        block,
-    }
+  return {
+    receipt,
+    transaction,
+    trace,
+    block,
+  }
 }
 
 async function main() {
-    const hash = process.argv[2]
-    const rpcUrl = process.argv[3] ?? RPC
+  const hash = process.argv[2]
+  const rpcUrl = process.argv[3] ?? RPC
 
-    if (hash == null) {
-        console.log('Please provide a transaction hash')
-        return
-    }
+  if (hash == null) {
+    console.log('Please provide a transaction hash')
+    return
+  }
 
-    const mock = await createMock(hash, rpcUrl)
+  const mock = await createMock(hash, rpcUrl)
 
-    if (mock == null) return
+  if (mock == null) return
 
-    if (mock.block != null) {
-        fs.writeFileSync(
-            `./test/mocks/tx/${Number(mock.transaction.blockNumber).toString()}.json`,
-            JSON.stringify(mock.block, null, 2),
-        )
-    }
+  if (mock.block != null) {
+    fs.writeFileSync(
+      `./test/mocks/tx/${Number(mock.transaction.blockNumber).toString()}.json`,
+      JSON.stringify(mock.block, null, 2),
+    )
+  }
 
-    delete mock.block
+  delete mock.block
 
-    fs.writeFileSync(`./test/mocks/tx/${hash.toLowerCase()}.json`, JSON.stringify(mock, null, 2))
+  fs.writeFileSync(`./test/mocks/tx/${hash.toLowerCase()}.json`, JSON.stringify(mock, null, 2))
 }
 
 main()
