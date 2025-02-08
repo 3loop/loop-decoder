f8aef97de8d09901cde5d00b7407ec8bebbbe5c7
Gheorghe Pinzaru <feross95@gmail.com>
2025-02-08 10:23:22 +0700

    Make in memory stores more flexible

--- packages/transaction-decoder/src/in-memory/contract-meta-store.ts
+++ packages/transaction-decoder/src/in-memory/contract-meta-store.ts
@@ -1,17 +1,14 @@
 import type { ContractData } from '../types.js'
-import { ContractMetaStore, ERC20RPCStrategyResolver, NFTRPCStrategyResolver, PublicClient } from '../effect.js'
+import { ContractMetaStore } from '../effect.js'
 import { Effect, Layer } from 'effect'
 
 const contractMetaCache = new Map<string, ContractData>()
 
-export const InMemoryContractMetaStoreLive = Layer.effect(
-  ContractMetaStore,
-  Effect.gen(function* () {
-    const publicClient = yield* PublicClient
-    const erc20Loader = ERC20RPCStrategyResolver(publicClient)
-    const nftLoader = NFTRPCStrategyResolver(publicClient)
-    return ContractMetaStore.of({
-      strategies: { default: [erc20Loader, nftLoader] },
+export const make = (strategies: ContractMetaStore['strategies']) =>
+  Layer.succeed(
+    ContractMetaStore,
+    ContractMetaStore.of({
+      strategies,
       get: ({ address, chainID }) =>
         Effect.sync(() => {
           const key = `${address}-${chainID}`.toLowerCase()
@@ -37,6 +34,5 @@ export const InMemoryContractMetaStoreLive = Layer.effect(
             contractMetaCache.set(key, result.result)
           }
         }),
-    })
-  }),
-)
+    }),
+  )
