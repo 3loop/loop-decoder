28a277689c28506b85391d822dc56e0a951d3362
Anastasia Rodionova <argali96@gmail.com>
2025-09-26 18:11:15 +0200

    Unify common dex routers interpretations

--- packages/transaction-interpreter/interpreters/aerodrom.ts
+++
@@ -1,9 +0,0 @@
-import { genericSwapInterpreter } from './std.js'
-import type { InterpretedTransaction } from '@/types.js'
-import type { DecodedTransaction } from '@3loop/transaction-decoder'
-
-export function transformEvent(event: DecodedTransaction): InterpretedTransaction {
-  return genericSwapInterpreter(event)
-}
-
-export const contracts = ['8453:0x6cb442acf35158d5eda88fe602221b67b400be3e']
--- packages/transaction-interpreter/interpreters/metamaskRouter.ts
+++
@@ -1,9 +0,0 @@
-import { genericSwapInterpreter } from './std.js'
-import type { InterpretedTransaction } from '@/types.js'
-import type { DecodedTransaction } from '@3loop/transaction-decoder'
-
-export function transformEvent(event: DecodedTransaction): InterpretedTransaction {
-  return genericSwapInterpreter(event)
-}
-
-export const contracts = ['1:0x881D40237659C251811CEC9c364ef91dC08D300C']
